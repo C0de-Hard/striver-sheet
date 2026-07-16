398c685c180b7efab26ecf9580e9f7728a50647e
Suraj Yadav <suraj.yadav.ug23@nsut.ac.in>
2026-07-17 01:10:04 +0530

    Time: 0 ms (100%), Space: 13.4 MB (64.16%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n = nums.size();//will be in form 3m + 1
-        int ans = 0;
-        for (int i = 0; i < 32; i++) {
-            int count_set_bits = 0;
-            for (int j = 0; j < n; j++) {
-                if (nums[j] & (1 << i)) count_set_bits++;
-            }
-            if (count_set_bits%3 == 0); //do nothing
-            else if (count_set_bits%3 == 1) ans = ans | (1 << i);
-            
+        int ones = 0;//counter ones for each bit
+        int twos = 0;//counter twos for each bit
+        for (int x : nums) {
+            ones = (ones ^ x) & ~twos;
+            twos = (twos ^ x) & ~ones;
         }
-        return ans;
+        cout << twos;
+        return ones;
     }
 };
