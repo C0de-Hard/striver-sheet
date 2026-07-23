d5bdfa28afa7fd7842d4543c11fe50968b7190b0
Suraj Yadav <suraj.yadav.ug23@nsut.ac.in>
2026-07-24 03:36:49 +0530

    Time: 0 ms (100%), Space: 11 MB (28.04%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,28 +1,24 @@
 class Solution {
     vector<vector<int>> ans;
 public:
-    void perms(vector<int>& nums, vector<bool>& vis, vector<int>& curr, int i = 0) {
+    void perms(vector<int>& nums, int swap_idx = 0) {
         int n = nums.size();
-        if (i == n) {
-            ans.push_back(curr);
+        if (swap_idx == n) {
+            ans.push_back(nums);
             return;
         }
-        for (int j = 0; j < n; j++) {
-            if (!vis[j]) {
-                vis[j] = true;
-                curr[i] = nums[j];
-                perms(nums,vis,curr,i+1);
-                vis[j] = false;
-            }
+        for (int i = swap_idx; i < n; i++) {
+            swap(nums[swap_idx],nums[i]);
+            perms(nums,swap_idx+1);
+            swap(nums[swap_idx],nums[i]);
         }
+
 
     }
     vector<vector<int>> permute(vector<int>& nums) {
         ans.clear();
         int n = nums.size();
-        vector<bool> vis(n,false);
-        vector<int> curr(n);
-        perms(nums,vis,curr);
+        perms(nums);
         return ans;
     }
 };
